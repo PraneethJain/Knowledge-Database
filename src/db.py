8de45d53cd2687098d76259d360daa2e5d84cf8e
PraneethJain <praneethjain005@gmail.com>
2023-12-02 23:39:57 +0530

    feat: remove error handling

--- src/db.py
+++ src/db.py
@@ -22,7 +22,7 @@ class DatabaseConnector:
         )
         self.cur = await self._conn.cursor()
         if (len(await self.get_table_names()) == 0):
-            await self.create_and_populate(self)
+            await self.create_and_populate()
 
     async def execute(self, query: str):
         await self.cur.execute(query)
@@ -40,119 +40,83 @@ class DatabaseConnector:
         return [r[0] for r in list(await self.fetchall())]
 
     async def get_primary_key(self, table: str) -> list[str]:
-        try:
-            return [
-                result[0]
-                for result in filter(
-                    lambda result: result[3] == "PRI", await self.table_desc(table)
-                )
-            ]
-        except Exception as E:
-            print(E)
+        return [
+            result[0]
+            for result in filter(
+                lambda result: result[3] == "PRI", await self.table_desc(table)
+            )
+        ]
 
     async def display_query(self, table: str) -> tuple[tuple, ...]:
         query_string = f"SELECT * FROM {table}"
         print(query_string)
-        try:
-            await self.execute(query_string)
-            return await self.fetchall()
-        except Exception as E:
-            print(E)
+        await self.execute(query_string)
+        return await self.fetchall()
 
     async def insert_query(self, table: str, query_attrs: list[str | None]) -> None:
         query_string = f'INSERT INTO {table} VALUES ({', '.join(map(lambda attr: f"'{attr}'" if attr is not None else "NULL", query_attrs))})'
         print(query_string)
-        try:
-            await self.execute(query_string)
-            return await self.fetchall()
-        except Exception as E:
-            print(E)
+        await self.execute(query_string)
+        return await self.fetchall()
 
     async def delete_query(self, table: str, query_attrs: list[str | None]):
         keys_list = await self.get_primary_key(table)
         query_string = f'DELETE FROM {table} WHERE {" AND ".join([f"{key}='{attr}'" for key, attr in zip(keys_list, query_attrs)])}'
         print(query_string)
-        try:
-            await self.execute(query_string)
-            print(self.description)
-            return await self.fetchall()
-        except Exception as E:
-            print(E)
+        await self.execute(query_string)
+        print(self.description)
+        return await self.fetchall()
 
     async def update_query(
-        self, table: str, query_attrs: list[str | None], update_tuple: (str, str | None)
+        self, table: str, query_attrs: list[str | None], update_tuple: tuple[str, str | None]
     ):
         keys_list = await self.get_primary_key(table)
         query_string = f'UPDATE {table} SET {update_tuple[0]}={f'"{update_tuple[1]}"' if update_tuple[1] is not None else "NULL"} WHERE {" AND ".join([f"{key}='{attr}'" for key, attr in zip(keys_list, query_attrs)])}'
         print(query_string)
-        try:
-            await self.execute(query_string)
-            print(self.description)
-            return await self.fetchall()
-        except Exception as E:
-            print(E)
+        await self.execute(query_string)
+        print(self.description)
+        return await self.fetchall()
 
-    async def table_desc(self, table: str) -> list[list[str]]:
+    async def table_desc(self, table: str) -> list[tuple]:
         query_string = f"DESC {table}"
-        try:
-            await self.execute(query_string)
-            results = await self.fetchall()
-            return results
-        except Exception as E:
-            print(E)
+        await self.execute(query_string)
+        results = await self.fetchall()
+        return results
 
     async def get_table_headers(self, table: str) -> list[str]:
-        try:
-            return [result[0] for result in await self.table_desc(table)]
-        except Exception as E:
-            print(E)
+        return [result[0] for result in await self.table_desc(table)]
 
-    async def last_year_awards(self) -> tuple[tuple, ...]:
-        try:
-            await self.execute("select * from Award WHERE YEAR(Date)=YEAR(CURDATE())")
-            Results = list(await self.fetchall())
-            Results.insert(0, tuple(await self.get_table_headers("Award")))
-            return Results
-        except Exception as E:
-            print(E)
+    async def last_year_awards(self) -> list[tuple]:
+        await self.execute("select * from Award WHERE YEAR(Date)=YEAR(CURDATE())")
+        Results = list(await self.fetchall())
+        Results.insert(0, tuple(await self.get_table_headers("Award")))
+        return Results
 
-    async def get_prerequisites(self, topic_name: str) -> tuple[(str)]:
-        try:
-            await self.execute(f"SELECT e.Name FROM Subtopic AS e INNER JOIN Subtopic eh ON eh.Name = e.Prerequisite_of_Name WHERE eh.Name='{topic_name}'")
-            Results = list(await self.fetchall())
-            Results.insert(0, ("Name",))
-            return Results
-        except Exception as E:
-            print(E)
+    async def get_prerequisites(self, topic_name: str) -> list[tuple]:
+        await self.execute(f"SELECT e.Name FROM Subtopic AS e INNER JOIN Subtopic eh ON eh.Name = e.Prerequisite_of_Name WHERE eh.Name='{topic_name}'")
+        Results = list(await self.fetchall())
+        Results.insert(0, ("Name",))
+        return Results
 
     async def get_citations(self):
-        try:
-            await self.execute("select SSN, Count(CitedByPubID) FROM Person INNER JOIN Publication AS P ON P.PubID = Published GROUP BY SSN")
-            Results = list(await self.fetchall())
-            Results.insert(0, ("SSN", "Count of Citations"))
-            return Results
-        except Exception as E:
-            print(E)
+        await self.execute("select SSN, Count(CitedByPubID) FROM Person INNER JOIN Publication AS P ON P.PubID = Published GROUP BY SSN")
+        Results = list(await self.fetchall())
+        Results.insert(0, ("SSN", "Count of Citations"))
+        return Results
 
-    async def get_university_by_pref(self, pref: str):
-        try:
-            await self.execute(f"select * from University where Name like '{pref}%'")
-            Results = list(await self.fetchall())
-            Results.insert(0, tuple(await self.get_table_headers("University")))
-            return Results
-        except Exception as E:
-            print(E)
+    async def get_university_by_pref(self, pref: str) -> list[tuple]:
+        await self.execute(f"select * from University where Name like '{pref}%'")
+        Results = list(await self.fetchall())
+        Results.insert(0, tuple(await self.get_table_headers("University")))
+        return Results
 
-    async def get_awards_university_aos(self, university: str):
-        try:
-            await self.execute(f"select Subtopic_Name, COUNT(Sponsor) FROM Teaches AS T INNER JOIN Award AS A ON A.P_SSN=T.P_SSN WHERE U_Name='{university}' GROUP BY U_Name, Subtopic_Name")
-            Results = list(await self.fetchall())
-            Results.insert(0, ("Subtopic_Name", "Number of Awards"))
-            return Results
-        except Exception as E:
-            print(E)
+    async def get_awards_university_aos(self, university: str) -> list[tuple]:
+        await self.execute(f"select Subtopic_Name, COUNT(Sponsor) FROM Teaches AS T INNER JOIN Award AS A ON A.P_SSN=T.P_SSN WHERE U_Name='{university}' GROUP BY U_Name, Subtopic_Name")
+        Results = list(await self.fetchall())
+        Results.insert(0, ("Subtopic_Name", "Number of Awards"))
+        return Results
 
-    async def close(self):
+    async def close(self) -> None:
         await self.cur.close()
         await self._conn.commit()
         self._conn.close()
